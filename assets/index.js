03541c9be5dae22f882d9f48740efab5d57eeb71
Mexil77 <mendel_77@hotmail.com>
2021-05-16 15:33:49 -0500

    Agregado el boton de mute y el primer plugin

---
+++ assets/index.js
@@ -0,0 +1,11 @@
+import MediaPlayer from "./MediaPlayer.js";
+import AutoPlay from "./plugins/AutoPlay.js";
+
+const video = document.querySelector("video");
+const buttonPlay = document.querySelector("#play");
+const buttonMute = document.querySelector("#mute");
+
+const player = new MediaPlayer({ el: video, plugins: [new AutoPlay()] });
+
+buttonPlay.onclick = () => player.togglePlay();
+buttonMute.onclick = () => player.toggleMute();
